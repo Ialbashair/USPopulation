97eb72449e574326fd8c5719008407a7306781c6
Ibrahim Albashair <albashair.ibrahim@gmail.com>
2026-03-05 11:53:50 -0600

    JS and HTML done.

---
+++ script.js
@@ -0,0 +1,45 @@
+(function() {
+    const API = "https://api.datausa.io/tesseract/data.jsonrecords?cube=acs_yg_total_population_5&measures=Population&drilldowns=Year";
+    function loadPopulation() {
+        const xhr = new XMLHttpRequest();
+
+        xhr.open("GET", API, true);
+
+        xhr.onload = function () {
+            if (xhr.status == 200) {
+                const response = JSON.parse(xhr.responseText);
+                displayData(response.data);
+            } else {
+                console.error("Request failed. Status: ", xhr.status);
+            }
+        };
+
+        xhr.onerror = function () {
+            console.log(1);
+            console.error("Netowrd error.");            
+        };
+
+        xhr.send();
+    }
+
+    function displayData(data) {
+        data.sort((a,b) => b.Year - a.Year);
+
+        const tableBody = document.querySelector("#populationTable tbody");
+
+        data.ForEach(item => {
+            const row = document.createElement("tr");
+            const year = document.createElement("td");
+            year.textContent = item.Year;
+
+            const population = document.createElement("td");
+            population.textContent = item.Population.toLocaleString();
+
+            row.appendChild(year);
+            row.appendChild(population);
+
+            tableBody.appendChild(row);
+        });
+    }
+    document.addEventListener("DOMContentLoaded", loadPopulation);
+})();
